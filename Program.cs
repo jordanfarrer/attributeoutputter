42dc1fe7bb68308b306be42fc82c376e78d6e1c2
jordanfarrer <jordan.farrer@gmail.com>
2014-04-16 11:49:50 -0600

    outputs index and value of each arugment passed in

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace ArgumentOutputter
 	{
 		static void Main(string[] args)
 		{
+			if (args.Length > 0)
+			{
+				for (var i = 0; i < args.Length; i++)
+				{
+					Console.WriteLine(string.Format("Argument {0}: {1}", i, args[i]));
+				}
+
+
+			}
+			else
+				Console.WriteLine("No arguments were passed.");
+
+			System.Threading.Thread.Sleep(5000);
 		}
 	}
 }
